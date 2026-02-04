ce83364b7e04c5a5285d04ab4f5e3c13bc3a6e15
zenedf <zenedf@protonmail.com>
2026-02-03 19:49:53 -0500

    It runs but still stores tasks in-memory

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,6 +4,8 @@ using BackendSimulator.Application.Interfaces;
 using BackendSimulator.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using BackendSimulator.Infrastructure.Data;
 
 var services = new ServiceCollection();
 
@@ -16,13 +18,24 @@ services.AddLogging(config =>
 // Application services
 services.AddSingleton<TaskService>();
 
-// Infrastructure
-services.AddSingleton<ITaskRepository, InMemoryTaskRepository>();
+// Infrastructure - EF Core with SQLite
+services.AddDbContext<BackendSimulatorDbContext>(options =>
+    options.UseSqlite("Data Source=backend.db"));
+
+services.AddScoped<ITaskRepository, InMemoryTaskRepository>();
 
 // Console UI
 services.AddSingleton<TaskConsoleRunner>();
 
+// Build the service provider
 var provider = services.BuildServiceProvider();
+
+// Ensure database is created and migrations are applied
+using (var scope = provider.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<BackendSimulatorDbContext>();
+    context.Database.Migrate();
+}
 
 var runner = provider.GetRequiredService<TaskConsoleRunner>();
 runner.Run();
---
+++ Infrastructure/Data/BackendSimulatorDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BackendSimulator.Infrastructure.Data;
+
+public class BackendSimulatorDbContextFactory : IDesignTimeDbContextFactory<BackendSimulatorDbContext>
+{
+    public BackendSimulatorDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<BackendSimulatorDbContext>();
+        optionsBuilder.UseSqlite("Data Source=backend.db");
+
+        return new BackendSimulatorDbContext(optionsBuilder.Options);
+    }
+}
