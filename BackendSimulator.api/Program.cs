14ebb387c56a0ecb5573828bd142c37b6226e341
zenedf <zenedf@protonmail.com>
2026-02-04 16:31:42 -0500

    configure DI for Application and Infrastructure extensions, create API DTOs

---
+++ BackendSimulator.api/Program.cs
@@ -0,0 +1,28 @@
+using BackendSimulator.Application;
+using BackendSimulator.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿
+using BackendSimulator.Application.Interfaces;
+using BackendSimulator.Infrastructure.Data;
+using BackendSimulator.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BackendSimulator.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Register infrastructure services here
+        services.AddDbContext<BackendSimulatorDbContext>(options =>
+            options.UseSqlite(configuration.GetConnectionString("Default")));
+
+        services.AddScoped<ITaskRepository, EfTaskRepository>();
+
+        return services;
+    }
+}
